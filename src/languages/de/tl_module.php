a273376288cbdcfd3d892441fd0cb9bcf6dc13d6
Andreas Schempp <andreas.schempp@terminal42.ch>
2014-05-01 08:56:56 +0200

    Clean up and fix language files

---
+++ src/languages/de/tl_module.php
@@ -0,0 +1,27 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * Twitter Reader for Contao Open Source CMS
+ *
+ * Copyright (C) 2013 Stefan Lindecke <lindesbs@googlemail.com>
+ *
+ * @package     twitterreader
+ * @license     http://gplv3.fsf.org/ GPL
+ * @filesource  https://github.com/lindesbs/TwitterReader
+ */
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_module']['twitterusers']                    = array('Name des Twitter Benutzers', 'Geben Sie hier den Benutzernamen ihres Twitter Kontos an');
+$GLOBALS['TL_LANG']['tl_module']['twittercount']                    = array('Anzahl der Nachrichten', 'Wieviele Nachrichten moechten Sie anzeigen ?');
+$GLOBALS['TL_LANG']['tl_module']['twitterEnableHTTPLinks']          = array('Wandel HTTP Links um', 'Sollen URL automatisch als Link umgeschrieben werden ?');
+$GLOBALS['TL_LANG']['tl_module']['twitterEnableUserProfileLink']    = array('Verlinkung des Benutzerprofils', 'Sollen automatisch vorhandene Benutzernamen zu den Twitterprofilen verlinkt werden?');
+$GLOBALS['TL_LANG']['tl_module']['twitterEnableHashtagLink']        = array('Verlinkung der Hashtags', 'Sollen automatisch vorhandene Hashtags verlinkt werden?');
+$GLOBALS['TL_LANG']['tl_module']['twittertemplate']                 = array('Template', 'Frontendtemplate. Beginnt mit twitterreader_');
+$GLOBALS['TL_LANG']['tl_module']['twitter_requesttype']             = array('Anfragetype', 'Wählen Sie welche Twitter-Meldungen ausgegeben werden sollen.');
+
+/**
+ * Miscellaneous
+ */
+$GLOBALS['TL_LANG']['tl_module']['twitterreader_auth_missing']      = 'Twitter Authentifizierung nicht vollstaendig. Bitte neu durchfuehren.';
--- src/languages/en/modules.php
+++ src/languages/en/modules.php
@@ -11,48 +11,6 @@
  */
 
 /**
- * Back end modules
+ * Front end modules
  */
-$GLOBALS['TL_LANG']['FMD']['twitterreader'] = array
-(
-	'Show twitter postings', ''
-);
-$GLOBALS['TL_LANG']['tl_module']['twitterusers'] = array
-(
-	'Name of twitter user',
-	'The name of the twitter user'
-);
-$GLOBALS['TL_LANG']['tl_module']['twittercount'] = array
-(
-	'Amount of postings',
-	'How many postings would you like to display ?'
-);
-$GLOBALS['TL_LANG']['tl_module']['twitterEnableHTTPLinks'] = array
-(
-	'Enable linking of HTTP',
-	'Linking of URL'
-);
-$GLOBALS['TL_LANG']['tl_module']['twitterEnableUserProfileLink'] = array
-(
-	'Enable linking of user',
-	'Linking of user tag'
-);
-$GLOBALS['TL_LANG']['tl_module']['twitterEnableHashtagLink'] = array
-(
-	'Enable linking of hashtags',
-	'Linking of hashtag'
-);
-$GLOBALS['TL_LANG']['tl_module']['twittertemplate'] = array
-(
-	'Template',
-	'Used Template for frontend rendering. Starts with twitterreader_'
-);
-$GLOBALS['TL_LANG']['tl_module']['twitter_requesttype'] = array
-(
-	'Requesttype',
-	'Different request types for tweets'
-);
-
-
-
-$GLOBALS['TL_LANG']['tl_module']['twitterreader_auth_missing'] = 'Twitter authentification not completed. Please reauthenticate.';
+$GLOBALS['TL_LANG']['FMD']['twitterreader'] = array('Show twitter postings');
