f278d339c012486a66f13383d593ae280153b237
Cliff Parnitzky <github@cliff-parnitzky.de>
2015-12-19 19:36:20 +0100

    Reorder fields

--- src/config/database.sql
+++ src/config/database.sql
@@ -1,14 +1,14 @@
 
 CREATE TABLE `tl_module` (
+  `twitter_requesttype` varchar(255) NOT NULL default 'user_timeline',
   `twitterusers` varchar(255) NOT NULL default '',
   `twittercount` varchar(255) NOT NULL default '',
-  `twitter_requesttype` varchar(255) NOT NULL default 'user_timeline',
-  `twittertemplate` varchar(255) NOT NULL default '',
 
-  `twitterEnableHTTPLinks` varchar(1) NOT NULL default '',
-  `twitterEnableMediaLinks` varchar(1) NOT NULL default '',
+  `twittertemplate` varchar(255) NOT NULL default '',
   `twitterEnableUserProfileLink` varchar(1) NOT NULL default '',
   `twitterEnableHashtagLink` varchar(1) NOT NULL default '',
+  `twitterEnableHTTPLinks` varchar(1) NOT NULL default '',
+  `twitterEnableMediaLinks` varchar(1) NOT NULL default '',
 
   `twitterLastUpdate` int(10) unsigned NOT NULL default '0',
   `twitterFeedBackup` text NULL,
