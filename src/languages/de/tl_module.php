ca0a49747df481a14b8c335e215b4545c710c7df
Cliff Parnitzky <github@cliff-parnitzky.de>
2015-12-19 19:42:44 +0100

    Reorder fields

--- src/languages/de/tl_module.php
+++ src/languages/de/tl_module.php
@@ -13,14 +13,14 @@
 /**
  * Fields
  */
+$GLOBALS['TL_LANG']['tl_module']['twitter_requesttype']                         = array('Anfragetyp', 'Wählen Sie, welche Twitter-Meldungen ausgegeben werden sollen.');
 $GLOBALS['TL_LANG']['tl_module']['twitterusers']                                = array('Name des Twitter Benutzers', 'Geben Sie hier den Benutzernamen ihres Twitter Kontos an.');
 $GLOBALS['TL_LANG']['tl_module']['twittercount']                                = array('Anzahl der Nachrichten', 'Wieviele Nachrichten möchten Sie anzeigen?');
-$GLOBALS['TL_LANG']['tl_module']['twitterEnableHTTPLinks']                      = array('Wandel HTTP Links um', 'Sollen URLs automatisch als Link umgeschrieben werden?');
-$GLOBALS['TL_LANG']['tl_module']['twitterEnableMediaLinks']                     = array('Wandel Media Links um', 'Sollen URLs zu Medien automatisch als Link umgeschrieben werden?');
+$GLOBALS['TL_LANG']['tl_module']['twittertemplate']                             = array('Template', 'Das verwendete Frontend-Template (beginnt mit <i>twitterreader_</i>).');
 $GLOBALS['TL_LANG']['tl_module']['twitterEnableUserProfileLink']                = array('Verlinkung des Benutzerprofils', 'Sollen vorhandene Benutzernamen automatisch zu den Twitterprofilen verlinkt werden?');
 $GLOBALS['TL_LANG']['tl_module']['twitterEnableHashtagLink']                    = array('Verlinkung der Hashtags', 'Sollen vorhandene Hashtags automatisch verlinkt werden?');
-$GLOBALS['TL_LANG']['tl_module']['twittertemplate']                             = array('Template', 'Das verwendete Frontend-Template (beginnt mit <i>twitterreader_</i>).');
-$GLOBALS['TL_LANG']['tl_module']['twitter_requesttype']                         = array('Anfragetyp', 'Wählen Sie, welche Twitter-Meldungen ausgegeben werden sollen.');
+$GLOBALS['TL_LANG']['tl_module']['twitterEnableHTTPLinks']                      = array('Wandel HTTP Links um', 'Sollen URLs automatisch als Link umgeschrieben werden?');
+$GLOBALS['TL_LANG']['tl_module']['twitterEnableMediaLinks']                     = array('Wandel Media Links um', 'Sollen URLs zu Medien automatisch als Link umgeschrieben werden?');
 
 /**
  * References
